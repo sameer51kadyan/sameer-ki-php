8e06017e2ac2c535e6f7031643a3de118b8a17a5
sameer51kadyan <sameer51kadyan@gmail.com>
2025-03-03 15:55:00 +0530

    Octal to string continue

--- strings.php
+++ strings.php
@@ -33,6 +33,26 @@
                 -> confusing escape character is \f (form feed).
                 -> Most interesting escape character is \ooo(Octal value convert into string)
                     and \xhh(hex value convert into string).
+
+
+        Important -> 
+                    -> \ooo :- Basically this is escaped character is one of the favorite 
+                                because we write octal no. in the string with the help of \(backslash) and php provide us actual string.
+                    Example :- suppose that we want write Sameer Kadyan.
+                               We will help of ASCII value 
+
+                               S = 83,  a = 97, m = 109, e = 101, r = 114, sapce = 32;
+                               K = 75,  a = 97, d = 100, y = 121, a = 97, n = 110;
+
+                               Dekh bhai Hum Octal numbers se strings likhenge aur ye jo uper values likhi hui hai ye ASCII table li hui hai to iska matlab ye hai ki ye decimal ki form me hai aur hame inhe octal me convert karna padega.
+
+
+                               alphabet     Decimal No.        Octal No.
+
+                                  S             83                123
+                                  a             97                141
+                                  m             109               155
+                                  e             101               145
                 
         
 
