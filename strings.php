f84db72789a54123f8d56a590307f02f2853e592
sameer51kadyan <sameer51kadyan@gmail.com>
2025-03-03 13:47:16 +0530

    first commit

---
+++ strings.php
@@ -0,0 +1,44 @@
+<?php
+/*
+        A strings literals can be specified in four diffrent way :-
+
+        -> single quotes.
+        -> double quotes.
+        -> heredoc syntax.
+        -> newdoc syntax.
+
+    */
+
+/*
+        Single quotes :-
+                        single quotes don't consider any variable just print straight forword.
+        Ex. 
+            $x = 5;
+            echo 'The number is $x';
+                                        Output:- The number is $x.
+
+    */
+
+/*
+        double quotes :-
+                        double quotes consider varibles and put that value with the strings.
+        Ex.
+            $x = 5;
+            echo "The number is $x ;
+                                        Output:- The number is 5.
+    
+        Note :- 
+                -> escaped character start with backslash(\);
+                -> escape character are many like \t(tab), \n(new line), etc.
+                -> confusing escape character is \f (form feed).
+                -> Most interesting escape character is \ooo(Octal value convert into string)
+                    and \xhh(hex value convert into string).
+                
+        
+
+
+        
+        
+    */
+
+?>
